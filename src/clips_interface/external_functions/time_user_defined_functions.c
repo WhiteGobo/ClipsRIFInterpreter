305f4d9f7c79f261f6cf102b3cca673451820025
WhiteGobo <richardfechner@posteo.net>
2024-12-19 13:41:53 +0100

    init time builtins

--- src/clips_interface/external_functions/time_user_defined_functions.c
+++ src/clips_interface/external_functions/time_user_defined_functions.c
@@ -1,1 +1,261 @@
 #include "time_user_defined_functions.h"
+
+#define RETURNFAIL(failure) \
+		Writeln(env, failure);\
+		SetErrorValue(env, &(CreateString(env, failure)->header));\
+		out->voidValue = VoidConstant(env);\
+		return;
+
+void rif_is_literal_date(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_is_literal_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_is_literal_dateTimeStamp(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_is_literal_time(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_is_literal_dayTimeDuration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_is_literal_yearMonthDuration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_is_literal_not_date(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_is_literal_not_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_is_literal_not_dateTimeStamp(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_is_literal_not_time(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_is_literal_not_dayTimeDuration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_is_literal_not_yearMonthDuration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_year_from_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_month_from_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_day_from_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_hours_from_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_minutes_from_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_seconds_from_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_year_from_date(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_month_from_date(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_day_from_date(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_hours_from_time(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_minutes_from_time(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_seconds_from_time(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_timezone_from_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_timezone_from_date(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_timezone_from_time(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_years_from_duration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_months_from_duration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_days_from_duration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_hours_from_duration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_minutes_from_duration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_seconds_from_duration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_subtract_dateTimes(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_subtract_dates(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_subtract_times(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_add_yearMonthDurations(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_subtract_yearMonthDurations(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_multiply_yearMonthDuration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_divide_yearMonthDuration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_divide_yearMonthDuration_by_yearMonthDuration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_add_dayTimeDurations(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_subtract_dayTimeDurations(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_multiply_dayTimeDuration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_divide_dayTimeDuration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+
+void rif_divide_dayTimeDuration_by_dayTimeDuration(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_add_yearMonthDuration_to_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_add_yearMonthDuration_to_date(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_add_dayTimeDuration_to_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_add_dayTimeDuration_to_date(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_add_dayTimeDuration_to_time(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_subtract_yearMonthDuration_from_date(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_subtract_yearMonthDuration_from_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_subtract_dayTimeDuration_from_dateTime(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_subtract_dayTimeDuration_from_date(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_subtract_dayTimeDuration_from_time(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_dateTime_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_dateTime_less_than(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_dateTime_greater_than(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_date_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_date_less_than(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_date_greater_than(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_time_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_time_less_than(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_time_greater_than(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_duration_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_yearMonthDuration_less_than(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_yearMonthDuration_greater_than(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_dayTimeDuration_less_than(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_dayTimeDuration_greater_than(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_dateTime_not_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_dateTime_less_than_or_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_dateTime_greater_than_or_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_date_not_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_date_less_than_or_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_date_greater_than_or_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_time_not_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_time_less_than_or_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_time_greater_than_or_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_duration_not_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_yearMonthDuration_less_than_or_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_yearMonthDuration_greater_than_or_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_dayTimeDuration_less_than_or_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
+void rif_dayTimeDuration_greater_than_or_equal(Environment *env, UDFContext *udfc, UDFValue *out){
+RETURNFAIL("not implemented");
+}
