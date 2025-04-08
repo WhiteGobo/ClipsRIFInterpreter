a12a12b59d2d602a386b6664972baf8c9bf40674
WhiteGobo <richardfechner@posteo.net>
2025-04-08 22:07:05 +0200

    added automatic builtin model creation in cmake

---
+++ mycreate.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+#cp build/builtin_resources/modelA.clp src/clips_interface/graph_models/
+cp build/builtin_resources/modelAchecker.clp src/clips_interface/graph_models/
---
+++ src/builtin_resources/recreate_builtin_graph_model.cmake
@@ -0,0 +1,14 @@
+set(MODELA "${CMAKE_CURRENT_BINARY_DIR}/modelA.clp")
+add_custom_command(OUTPUT ${MODELA}
+	COMMAND generate_builtin_model "--modelA" > " ${MODELA}"
+	DEPENDS generate_builtin_model
+)
+
+#add_dependencies(hello_test ${MODELA})
+
+#install(FILES ${MODELA}
+#	TYPE DATA
+#)
+
+
+#message(FATAL_ERROR "brubru ${MODELA}")
