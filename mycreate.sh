728ee65306c4c50587f2b1b5718392cca304cfc7
WhiteGobo <richardfechner@posteo.net>
2025-04-15 12:11:45 +0200

    added exit on failure to mycreate.sh

--- mycreate.sh
+++ mycreate.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
+set -e #exit on failure
+set -x
+
 #cp build/builtin_resources/modelA.clp src/clips_interface/graph_models/
 cp build/builtin_resources/modelAchecker.clp src/clips_interface/graph_models/
