0608cf37f6fe1a5b898450a8964069e93a603167
WhiteGobo <richardfechner@posteo.net>
2025-05-21 09:44:13 +0200

    temporary safe. nothing changed

--- mycreate.sh
+++ mycreate.sh
@@ -4,4 +4,4 @@ set -e #exit on failure
 set -x
 
 cp build/builtin_resources/modelA.clp src/clips_interface/graph_models/
-#cp build/builtin_resources/modelAchecker.clp src/clips_interface/graph_models/
+cp build/builtin_resources/modelAchecker.clp src/clips_interface/graph_models/
