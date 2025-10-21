c7865d3e28aa7ac0faa9483a681cc5ba79ab8016
Richard Focke Fechner <richardfechner@posteo.net>
2025-10-21 14:27:45 +0200

    removed unused src/assets/. fixed memory leak in crifi_graph_debug_set_random_strategy

--- src/clips_interface/graph_models/crifi_graph_models_debug.c
+++ src/clips_interface/graph_models/crifi_graph_models_debug.c
@@ -2,8 +2,11 @@
 #include "crifi_import.h"
 
 void crifi_graph_debug_set_random_strategy(crifi_graph* graph, int seed){
+	struct DynamicValue retval;
 	char seedcmd[50];
 	sprintf(seedcmd, "(seed %d)", seed);
-	eval(graph, seedcmd);
-	eval(graph, "(set-strategy random)");
+	retval = eval(graph, seedcmd);
+	free_dynamic_value(retval);
+	retval = eval(graph, "(set-strategy random)");
+	free_dynamic_value(retval);
 }
